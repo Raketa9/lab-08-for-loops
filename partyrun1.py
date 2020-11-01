b3096f0e99cd14e061b6a39c9b54f12f85e66ad7
zarina barsh <zarinka.barsh@gmail.com>
2020-10-31 23:52:50 -0400

    html update

--- partyrun1.py
+++ partyrun1.py
@@ -1,7 +1,7 @@
 input("Let's party...")
 num = int(input("How long 'till the party?:"))
 range = (num, 1, - 1)
-for (int index= num-1:)
+for (int(input -> num -1))
 
 print (range)
 input("PARTY TIME!!!")
